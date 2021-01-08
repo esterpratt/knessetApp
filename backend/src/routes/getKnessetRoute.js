30308ca463faf1c8f590c8af7f7d092bdd63bc4f
ester.pratt <ester.pratt@naturalint.com>
2021-01-08 16:44:39 +0200

    add mariadb

--- backend/src/routes/getKnessetRoute.js
+++ backend/src/routes/getKnessetRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const pool = require('../db');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello');
+router.get('/', async (req, res) => {
+  const connection = await pool.getConnection();
+  const rows = await connection.query("SELECT 1 as val");
+  res.send(`Hello: ${rows}`);
 });
 
 module.exports = { router };
